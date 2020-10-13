387a52b5360202b730abd50d50248ad6a7389afc
Ritik Kumar <ritik.kumar006@gmail.com>
2020-10-13 09:29:23 +0530

    Added post delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post")
 const auth = require("../../middleware/auth");
 const request = require("request");
 const config = require("config");
@@ -150,6 +151,12 @@ router.get("/user/:user_id", async (req, res) => {
 // @access Private
 router.delete("/", auth, async (req, res) => {
   try {
+    // Remove user posts
+    await Post.deleteMany({user: req.user.id});
+
+
+
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
