b8ed73f8f572202f79dba200ace3a154a10b4f09
muskan278 <agarwalmuskan278@gmail.com>
2020-10-26 10:49:53 +0530

    Front end and linking done

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,8 @@ import React,{Fragment,useEffect} from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Alert from './components/layout/Alert';
+import Routes from './components/routing/Routes';
 
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-import CreateProfile from './components/profile-forms/CreateProfile'
-import EditProfile from './components/profile-forms/EditProfile'
-import AddExperience from './components/profile-forms/AddExperience'
-import AddEducation from './components/profile-forms/AddEducation'
 //Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -35,19 +27,13 @@ return(
 <Router>
 <Fragment>
 <Navbar/>
-<Route exact path='/' component={Landing}/>
-<section className="container">
-<Alert />
+
 <Switch>
-<Route exact path='/register' component={Register}/>
-<Route exact path='/login' component={Login}/>
-<PrivateRoute exact path='/dashboard' component={Dashboard}/>
-<PrivateRoute exact path='/create-profile' component={CreateProfile}/>
-<PrivateRoute exact path='/edit-profile' component={EditProfile}/>
-<PrivateRoute exact path='/add-education' component={AddEducation}/>
-<PrivateRoute exact path='/add-experience' component={AddExperience}/>
+<Route exact path="/" component={Landing} />
+<Route component={Routes} />
 </Switch>
-</section>
+
+
 </Fragment>
 </Router>
 </Provider>
