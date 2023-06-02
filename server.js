f1932693fe74e0d950cb12c8c9e2975fb9d72b58
Ritik Kumar <ritik.kumar006@gmail.com>
2023-06-02 20:44:21 +0530

    update project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const connectDB = require("./config/db");
-const path=require('path');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -20,18 +21,16 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-
 //Serve static assets in production
-if(process.env.NODE_ENV==='production')
-{
-    //set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-const port = process.env.PORT || "5000"
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
